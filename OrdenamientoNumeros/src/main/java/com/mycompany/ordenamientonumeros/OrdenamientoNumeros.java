e699102b5bc042332089e31fe3cf7e9ea3b473ae
Eduardo9494 <1433341@senati.pe>
2023-08-17 19:46:13 -0500

    se muestra un encabezado y luego se imprime cada numero ordenado de nemor a mayor

--- OrdenamientoNumeros/src/main/java/com/mycompany/ordenamientonumeros/OrdenamientoNumeros.java
+++ OrdenamientoNumeros/src/main/java/com/mycompany/ordenamientonumeros/OrdenamientoNumeros.java
@@ -6,23 +6,28 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class OrdenamientoNumeros {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         int[] numeros = new int[5];
         
-        System.out.println("==========================================");
-        System.out.println("======== Algoritmo De Ordenamiento =======");
-        System.out.println("==========================================");
-        for (int i = 0; i < 5; i++){
-            System.out.println("Ingrese el numero" + (i+1) + "; ");
+        // Ingreso de los cinco números
+        System.out.println("===================================================");
+        System.out.println("=============  Algoritmo De Ordenamiento ==========");
+        System.out.println("===================================================");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Ingrese el número " + (i+1) + ": ");
             numeros[i] = scanner.nextInt();
         }
         
+        // Ordenamiento de los números
         Arrays.sort(numeros);
         
+        // Mostrar los números ordenados de menor a mayor
+        System.out.println("==================================");
+        System.out.println("Números ordenados de menor a mayor:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(numeros[i]);
+        }
     }
-    {
-        
-    }
-    }
+}
