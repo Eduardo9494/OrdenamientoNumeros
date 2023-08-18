ba86d980480060ef0ee3339dee08bb98facadb79
Eduardo9494 <1433341@senati.pe>
2023-08-17 19:30:37 -0500

    se crea una instancia del objeto Scanner para leer la entrada del usuario

--- OrdenamientoNumeros/src/main/java/com/mycompany/ordenamientonumeros/OrdenamientoNumeros.java
+++ OrdenamientoNumeros/src/main/java/com/mycompany/ordenamientonumeros/OrdenamientoNumeros.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 
 public class OrdenamientoNumeros {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+    }
     {
         
     }
